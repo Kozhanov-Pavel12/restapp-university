131139b57c5aa6b5f675fb44841d932ea4a0f4a8
Pavel <PKozhanov99@inbox.ru>
2024-04-10 17:32:44 +0300

    Исправить ошибку с переменной

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Suspense, memo, useCallback } from 'react'
 import { AppRouteProps, routeConfig } from '../shared/routerConfig'
 
